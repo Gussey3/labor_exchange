ec5bbe7f3b469d7ea82587c92b13bb3bbb5eb42b
DamirK <you@example.com>
2025-04-20 19:54:47 +0500

    feat: добавлены поля в бизнес модели

--- src/models/job.py
+++ src/models/job.py
@@ -1,6 +1,16 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+
+from models.response import Response
 
 
 @dataclass
 class Job:
     id: int
+    user_id: int
+    title: str
+    description: str
+    salary_from: str
+    salary_to: str
+    is_active: bool
+
+    responses: list[Response] = field(default_factory=list)
